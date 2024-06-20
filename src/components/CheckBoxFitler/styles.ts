c344f7086f92a2c18cf77cf3f91660b4e061d7ca
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-20 20:11:56 -0300

    removing files

---
+++ src/components/CheckBoxFitler/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+    headerText: {
+        color: "#ffffff",
+        
+    },
+    container: {
+       
+        flexDirection: 'row'
+        
+    }
+});
+
+export default styles;
